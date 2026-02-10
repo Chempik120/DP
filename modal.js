4227685b4d267f86a29f2111281835c3f583f020
Chempik120 <135869484+Chempik120@users.noreply.github.com>
2026-02-10 10:33:20 +0200

    s

--- modal.js
+++ modal.js
@@ -1,16 +1,29 @@
 (() => {
     const refs = {
-      openModalBtn: document.querySelector('[data-modal-open]'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
+        openModalBtn: document.querySelector('[data-modal-open]'),
+        closeModalBtn: document.querySelector('[data-modal-close]'),
+        modal: document.querySelector('[data-modal]'),
+        tabBtns: document.querySelectorAll('.tab-btn'),
+        formContainers: document.querySelectorAll('.form-container')
     };
-  
+
     refs.openModalBtn.addEventListener('click', toggleModal);
     refs.closeModalBtn.addEventListener('click', toggleModal);
-  
+
     function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
-      document.body.classList.toggle('no-scroll');
+        refs.modal.classList.toggle('is-hidden');
+        document.body.classList.toggle('no-scroll');
     }
-  })();
-  
+
+    refs.tabBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const targetTab = btn.dataset.tab;
+
+            refs.tabBtns.forEach(tab => tab.classList.remove('active'));
+            refs.formContainers.forEach(container => container.classList.remove('active'));
+            
+            btn.classList.add('active');
+            document.querySelector(`[data-form="${targetTab}"]`).classList.add('active');
+        });
+    });
+})();
